6bf68b44d0038df8be3530718921fdcfa23256fb
Fille <filipk73@gmail.com>
2025-02-11 18:23:25 +0100

    chore: test

---
+++ chat-test/chat-one.py
@@ -0,0 +1,30 @@
+# SPDX-License-Identifier: Apache-2.0
+
+from openai import OpenAI
+
+# Modify OpenAI's API key and API base to use vLLM's API server.
+openai_api_key = "EMPTY"
+openai_api_base = "http://host.docker.internal:30000/v1"
+
+client = OpenAI(
+    # defaults to os.environ.get("OPENAI_API_KEY")
+    api_key=openai_api_key,
+    base_url=openai_api_base,
+)
+
+models = client.models.list()
+model = models.data[0].id
+
+chat_completion = client.chat.completions.create(
+    messages=[{
+        "role": "system",
+        "content": "You are a helpful assistant."
+    }, {
+        "role": "user",
+        "content": "Tell me a story of 2000 words"
+    }, ],
+    model=model,
+)
+
+print("Chat completion results:")
+print(chat_completion)
